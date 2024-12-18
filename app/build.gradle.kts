e9d7a814b22e0973d0a8408b799076e50886b282
Mikhovich Anton <mikhovich.anton@gmail.com>
2024-12-18 23:33:28 +0300

    Made Updates on: -activity_layout -created a custom view -created a few basic methods to spin the WheelOfColorsView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -46,5 +49,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.android)
 }
--- app/src/main/java/com/android/aston_intensive_2/MainActivity.kt
+++ app/src/main/java/com/android/aston_intensive_2/MainActivity.kt
@@ -2,10 +2,59 @@ package com.android.aston_intensive_2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.android.aston_intensive_2.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var isSpinning: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            rotateButton.setOnClickListener {
+                if(!isSpinning) {
+                    spinTheWheel()
+                }
+            }
+        }
+
+    }
+
+    private fun spinTheWheel(){
+        isSpinning = true
+
+        val randomAngle = Random.nextInt(0, 720).toFloat()
+        val startingAngle = binding.wheelOfColorsView.currentAngle
+        rotatingWheel(startingAngle, randomAngle)
+    }
+
+    private fun rotatingWheel(startingAngle: Float, endingAngle: Float){
+        val step = 10f
+        var currentAngle = startingAngle
+
+        lifecycleScope.launch {
+            while (currentAngle < endingAngle){
+                withContext(Dispatchers.IO){
+                    binding.wheelOfColorsView.rotation()
+                    currentAngle += step
+                }
+                kotlinx.coroutines.delay(20)
+            }
+            stopTheWheel(endingAngle)
+        }
+    }
+
+    private fun stopTheWheel(endingAngle: Float) {
+        isSpinning = false
+        binding.wheelOfColorsView.settingCurrentAngle(endingAngle)
     }
 }
